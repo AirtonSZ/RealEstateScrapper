90e83e5f85d5276924c4e1d349f1d7fb3492c17f
Airton de Souza Oliveira <airtonszoliveira@gmail.com>
2025-05-07 22:00:25 -0300

    fix: enable only headless

--- main.py
+++ main.py
@@ -116,8 +116,8 @@ tic = time.time()
 options = Options()
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
 options.add_argument("--disable-blink-features=AutomationControlled")
-# options.add_argument('--headless')  # Run in headless mode
-# options.add_argument('--disable-gpu')  # Required for headless mode
+options.add_argument('--headless')  # Run in headless mode
+options.add_argument('--disable-gpu')  # Required for headless mode
 # options.add_argument('--no-sandbox')
 # options.add_argument('--disable-dev-shm-usage')
 
